f677c0cf874fd6b0b3cb6faa484228359ff295ff
jeremyd2019 <github@jdrake.com>
2021-08-26 17:39:50 -0700

    Update bla.py

--- bla.py
+++ bla.py
@@ -15,7 +15,7 @@ def process_package(pkgfile):
     with tempfile.TemporaryDirectory(dir="/c/_") as tmpdir:
         localfile = os.path.join(tmpdir, pkgfile)
         urlretrieve("https://mirror.msys2.org/mingw/mingw64/{}".format(pkgfile), localfile)
-        subprocess.check_call(['tar', '-C', tmpdir, '-xf', localfile])
+        subprocess.call(['bsdtar', '-C', tmpdir, '-xf', localfile], stderr=subprocess.DEVNULL)
         for root, dirs, files in os.walk(tmpdir):
             for name in files:
                 p = os.path.normpath(os.path.join(root, name))
