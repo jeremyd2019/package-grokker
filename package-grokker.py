c1dadaa0aadbb7152b89566dfb77d48823f99b6d
Jeremy Drake <github@jdrake.com>
2021-09-12 20:40:18 -0700

    use zstandard and tarfile instead of bsdtar.
    Much easier to manage, especially on Windows.  No temp files required.

--- package-grokker.py
+++ package-grokker.py
@@ -1,37 +1,60 @@
 import argparse
-import pefile
-import subprocess
-import sys
-import os
 import concurrent.futures
-import tempfile
-from urllib.request import urlretrieve
+import os
+import pefile
+import tarfile
+import threading
+import zstandard
+
+from contextlib import contextmanager
+from urllib.request import urlopen
 
 from pacdb import pacdb
 
+_tls = threading.local()
+
+@contextmanager
+def open_zstd_supporting_tar(name, fileobj):
+    # HACK: please, Python, support zst with :* in tarfile
+    # could probably check for magic, but would have to have a stream wrapper
+    # like tarfile already has to "put back" the magic bytes
+    if name.endswith(".zst"):
+        if not hasattr(_tls, 'zctx'):
+            _tls.zctx = zstandard.ZstdDecompressor()
+        with _tls.zctx.stream_reader(fileobj, closefd=False) as zstream:
+            with tarfile.open(fileobj=zstream, mode="r|") as tar:
+                yield tar
+    else:
+        with tarfile.open(fileobj=fileobj, mode="r|*") as tar:
+            yield tar
+
+
 class ProblematicImportSearcher(object):
-    def __init__(self, problem_dlls, problem_symbols, temp_dir=None, local_mirror=None):
+    def __init__(self, problem_dlls, problem_symbols, local_mirror=None):
         super(ProblematicImportSearcher, self).__init__()
         self.problem_dlls = set(dll.encode('ascii').lower() for dll in problem_dlls)
         self.problem_symbols = set(sym.encode('ascii') for sym in problem_symbols)
-        self.temp_dir = temp_dir
         self.local_mirror = local_mirror
 
+    def _open_package(self, pkg):
+        if self.local_mirror:
+            localfile = os.path.join(self.local_mirror, 'mingw', pkg.db.name, pkg.filename)
+            return open(localfile, "rb")
+        else:
+            return urlopen("https://mirror.msys2.org/mingw/{}/{}".format(pkg.db.name, pkg.filename))
+
     def __call__(self, pkg):
-        with tempfile.TemporaryDirectory(dir=self.temp_dir) as tmpdir:
-            if self.local_mirror:
-                localfile = os.path.join(self.local_mirror, 'mingw', pkg.db.name, pkg.filename)
-            else:
-                localfile = os.path.join(tmpdir, pkg.filename)
-                urlretrieve("https://mirror.msys2.org/mingw/{}/{}".format(pkg.db.name, pkg.filename), localfile)
-            subprocess.call(['bsdtar', '-C', tmpdir, '-xf', localfile], stderr=subprocess.DEVNULL)
-            for root, dirs, files in os.walk(tmpdir):
-                for name in files:
-                    p = os.path.normpath(os.path.join(root, name))
-                    if os.path.splitext(p)[-1] not in (".dll", ".exe", ".pyd"):
+        with self._open_package(pkg) as pkgfile:
+            with open_zstd_supporting_tar(pkg.filename, pkgfile) as tar:
+                for entry in tar:
+                    if not entry.isfile() or os.path.splitext(entry.name)[-1] not in (".dll", ".exe", ".pyd"):
                         continue
+
+                    with tar.extractfile(entry) as infofile:
+                        data = infofile.read()
+
                     try:
-                        pe = pefile.PE(p, fast_load=True)
+                        pe = pefile.PE(data=data, fast_load=True)
                         pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_IMPORT']])
                     except pefile.PEFormatError:
                         continue
@@ -50,12 +73,11 @@ parser.add_argument('-e', '--repo', default='mingw64', help='pacman repo name to
 parser.add_argument('-p', '--package', required=True, help='package from which to find dependents')
 parser.add_argument('-d', '--dll', required=True, action='append', help='dll(s) from which problematic symbols are imported')
 parser.add_argument('-l', '--local-mirror', help='root directory of local mirror')
-parser.add_argument('-t', '--tempdir', help='directory for temporary files')
 parser.add_argument('symbol', nargs='*', help='problematic symbol(s)')
 
 options = parser.parse_args()
 
-package_handler = ProblematicImportSearcher(options.dll, options.symbol, options.tempdir, options.local_mirror)
+package_handler = ProblematicImportSearcher(options.dll, options.symbol, options.local_mirror)
 
 if options.local_mirror:
     repo = pacdb.Database(options.repo, filename=os.path.join(options.local_mirror, 'mingw', options.repo, '{}.db'.format(options.repo)))
