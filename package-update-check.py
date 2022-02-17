af1838c927ae90b61bf943f29f72b4acead73c10
Jeremy Drake <github@jdrake.com>
2022-02-17 12:01:31 -0800

    output removed dlls/symbols too

--- package-update-check.py
+++ package-update-check.py
@@ -1,8 +1,21 @@
 import argparse
 import os
+import pprint
 
+from contextlib import contextmanager
 from pacdb import pacdb
 import grokkermod
+
+@contextmanager
+def gha_group(title):
+    print(f'\n::group::{title}')
+    try:
+        yield
+    finally:
+        print('::endgroup::')
+
+# hack pprint
+del pprint.PrettyPrinter._dispatch[bytes.__repr__]
 
 parser = argparse.ArgumentParser(description='Search packages for problematic imports')
 parser.add_argument('-e', '--repo', default='mingw64', help='pacman repo name to search')
@@ -25,7 +38,10 @@ elif options.repo == 'msys':
 else:
     repo = pacdb.mingw_db_by_name(options.repo, 'files')
 
-package_handler = grokkermod.ProblematicImportSearcher(grokkermod.diff_package_exports(*options.url), local_mirror)
+problem_dll_symbols = grokkermod.diff_package_exports(*options.url)
+with gha_group('Removed DLLs/Symbols'):
+    pprint.pprint(problem_dll_symbols)
+package_handler = grokkermod.ProblematicImportSearcher(problem_dll_symbols, local_mirror)
 
 for pkgbase in grokkermod.grok_dependency_tree(repo, options.package, package_handler):
     print(pkgbase)
